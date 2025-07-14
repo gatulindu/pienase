037add7272b5acb6f77ca039ad80c22fffa19846
Gatoli <gatulindu@keemail.me>
2025-07-14 16:08:37 -0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -2,11 +2,11 @@
 
 if [[ $1 == "--auto" ]]
 then
-	gcc *.c -o pie
-	sudo mv pie /usr/local/bin/
+	gcc *.c -o terp
+	sudo mv terp /usr/local/bin/
 else
 	read -e -p "Compiler? " CC
 	read -e -p "Install Dir? " DIR
-	$CC *.c -o ./pie
-	sudo mv pie $DIR
+	$CC *.c -o ./terp
+	sudo mv terp $DIR
 fi
