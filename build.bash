00fd264b5ad9ac2df8d66851d400e16d3ff8f41c
Gatoli <gatulindu@keemail.me>
2025-07-13 21:13:09 -0300

    Update build.bash

--- build.bash
+++ build.bash
@@ -2,11 +2,11 @@
 
 if [[ $1 == "--auto" ]]
 then
-	gcc src/*.c -o pie
+	gcc *.c -o pie
 	sudo mv pie /usr/local/bin/
 else
 	read -e -p "Compiler? " CC
 	read -e -p "Install Dir? " DIR
-	$CC src/*.c -o ./pie
+	$CC *.c -o ./pie
 	sudo mv pie $DIR
 fi
